4219ac1aa42453e79f55547f44e141e48efc1b97
jasonbrogan <jkbrogan@gmail.com>
2019-02-12 12:00:15 -0500

    Update SonicPiSampleSlicer.rb

--- SonicPiSampleSlicer.rb
+++ SonicPiSampleSlicer.rb
@@ -2,11 +2,24 @@
 
 # This slices the Amen loop sample into 8 slices of equal length and randomly plays through them
 
-live_loop :beat_slicer do
-  slice_idx = rand_i(8)
+live_loop :sample_slicer do
+  slice = rand_i(8)
   slice_size = 0.125
-  s = slice_idx * slice_size
+  s = slice * slice_size
   f = s + slice_size
   sample :loop_amen, start: s, finish: f
   sleep sample_duration :loop_amen, start: s, finish: f
+end
+
+# Another approach
+
+a = [file path of your sample]
+
+live_loop :sample_slicer do
+  slice = rand_i(8)
+  slice_size = 0.125
+  s = slice * slice_size
+  f = s + slice_size
+  sample a, beat_stretch: 4, start: s, finish: f
+  sleep 4 / slice
 end
